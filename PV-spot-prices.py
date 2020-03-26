77c450a993fb13a20a2fcd6626ff5b0810aba00b
Vladimir <petrov.vladm@gmail.com>
2020-03-26 22:23:31 +0100

    Minor changes

--- PV-spot-prices.py
+++ PV-spot-prices.py
@@ -16,7 +16,7 @@ from openpyxl.styles import Font
 from openpyxl.styles import Alignment
 import requests
 from bs4 import BeautifulSoup as bs
-        
+
 def get_data(url):
     """
     downloads spot price data and its date of update
@@ -35,7 +35,7 @@ def get_data(url):
         else:
             print("Something wrong with downloading Data. Please try again.")
     else:
-        print("Request was not successful")
+        print("Request was not successful.")
     return date_tag, tables_list
 
 def check_data_2(wb, date_str):
@@ -64,16 +64,16 @@ def datetag_to_datetime(date_tag):
     string_cut=string[0:string.find(",")]+string[string.find(",")+1:]
     date=dt.strptime(string_cut, '%B %d %Y')
     return date
-    
+
 def swap_headers(ws, header_old, header_new):
     """
-    changes columns names inside the given worksheet  
+    changes columns names within given worksheet
     """
     for row in ws.rows:
         for cell in row:
             if cell.value==header_old:
                 cell.value=header_new
-                
+
 def add_new_data_to_column(ws, df, date_str):
     """
     adds new data to the table
@@ -93,7 +93,7 @@ def create_main_tables_structure(ws, df, date_str):
     ws['A1']="Data is provided for reference purposes only. Data is the property of PV InfoLink.\nUsers should respect the legitimate rights for use based on the principle of integrity."
     ws['A1'].alignment=Alignment(wrap_text=True)
     ws.merge_cells('A1:D1')
-    ws['A2']="Please do not modify this sheet manually. Use a copy for playing with the data" 
+    ws['A2']="Please do not modify this sheet manually. Use a copy for playing with the data"
     ws.merge_cells('A2:D2')
     ws['A3']=df.columns[-3]+", "+ws.title
     ws['A3'].font=Font(bold=True)
@@ -104,20 +104,20 @@ def create_main_tables_structure(ws, df, date_str):
     ws.cell(row=ws.max_row, column=1).font=Font(bold=True)
     ws.merge_cells(start_row=ws.max_row, start_column=1, end_row=ws.max_row, end_column=ws.max_column)
     for r in dataframe_to_rows(df[["Category", "Item", "Unit", "Average Price"]], index=False, header=True):
-        ws.append(r)    
+        ws.append(r)
     ws.cell(row=ws.max_row+2, column=1, value=df.columns[-2]+", "+ws.title)
     ws.cell(row=ws.max_row, column=1).font=Font(bold=True)
     ws.merge_cells(start_row=ws.max_row, start_column=1, end_row=ws.max_row, end_column=ws.max_column)
     for r in dataframe_to_rows(df[["Category", "Item", "Unit", "Low Price"]], index=False, header=True):
         ws.append(r)
-        
+
     ws.cell(row=ws.max_row+2, column=1).value='*The quote of mono wafers is low resistivity product.'
     ws.merge_cells(start_row=ws.max_row, start_column=1, end_row=ws.max_row, end_column=ws.max_column)
     ws.cell(row=ws.max_row+1, column=1).value='**Mono-Si wafer quotes are based on those of 180µm. Prices of thinner ones are calculated with formula.'
     ws.merge_cells(start_row=ws.max_row, start_column=1, end_row=ws.max_row, end_column=ws.max_column)
     ws.cell(row=ws.max_row+1, column=1).value='***US and Indian module prices showed on the PV InfoLink website is after-tax price (punitive tariffs). Others are FOB price.'
     ws.merge_cells(start_row=ws.max_row, start_column=1, end_row=ws.max_row, end_column=ws.max_column)
-    
+
     swap_headers(ws, "Low Price", date_str)
     swap_headers(ws, "Average Price", date_str)
     swap_headers(ws, "High Price", date_str)
@@ -127,7 +127,7 @@ def create_main_tables_structure(ws, df, date_str):
     ws.row_dimensions[1].height=30
 
 
-#creating output file if needed 
+#creating output file if needed
 filename="Spot_prices.xlsx"
 output_file=Path.cwd() / filename
 if output_file.is_file():
@@ -170,9 +170,9 @@ df_all.reset_index(drop=True, inplace=True)
 df_all.rename(columns={'High':'High Price', 'Low':'Low Price','Average price':'Average Price'},  inplace=True)
 
 category_list=4*["Polysilicon"]+6*["Wafer"]+8*["Cell"]+4*["Module"]+6*["Module by region"]+["Module BOM Materials"]
-unit_list=4*["kg"]+6*["pc"]+18*["W"]+["m2"]  
+unit_list=4*["kg"]+6*["pc"]+18*["W"]+["m2"]
 
-currency_list=[]    
+currency_list=[]
 for i in range(len(df_all["Item"])):
     currency_list.append(df_all["Item"][i][-4:-1])
 # print(currency_list)
@@ -189,29 +189,29 @@ df_RMB=df_all[df_all["Currency"]=="RMB"]
 df_RMB.reset_index(drop=True, inplace=True)
 
 #adding new data to a new excel sheet
-if data_status == True:     
+if data_status == True:
     wb.create_sheet(date_str)
     ws_new=wb[date_str]
     for df in list_of_df:
         for r in dataframe_to_rows(df, index=False, header=True):
             ws_new.append(r)
         ws_new.append([])
-    print("Update date:", date_str)    
-    print("Data is added to the correcponding worksheet. Please check the file")
+    print("Update date:", date_str)
+    print("Data is added to the corresponding worksheet. Please check the file.")
 
 #adding new data to main tables (USD, RMB)
-if data_status == True: 
+if data_status == True:
     if ws_USD['A3'].value==df_USD.columns[-3]+", "+ws_USD.title:
         add_new_data_to_column(ws_USD, df_USD[["High Price","Average Price","Low Price"]],date_str)
         add_new_data_to_column(ws_RMB, df_RMB[["High Price","Average Price","Low Price"]],date_str)
     else:
         create_main_tables_structure(ws_USD, df_USD, date_str)
         create_main_tables_structure(ws_RMB, df_RMB, date_str)
-    
+
 wb.save(filename)
-      
+
 try:
     print("Thanks for using this tool. Ha en hyggelig dag!")
-    input("Press enter to exit")
+    input("Press enter to exit.")
 except SyntaxError:
     pass
